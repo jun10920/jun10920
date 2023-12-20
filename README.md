8709a37c9dec3a3d50ebd5946ab4b3b28b8cda9c
jun1090 <abide0206@naver.com>
2023-12-21 00:40:16 +0900

    블로그 5개까지 자동 갱신

---
+++ README.md
@@ -0,0 +1,18 @@
+## 낙관적 허무주의 개발자
+
+[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=abide0206)](https://solved.ac/abide0206/)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=jun10920&layout=compact&hide=r,jupyter%20notebook,c%23&exclude_repo=roharui.github.io)](https://github.com/anuraghazra/github-readme-stats)
+
+---
+
+## 📕 Latest Blog Posts
+
+<p>
+    <a href="https://jun10920.tistory.com/"><img src="https://img.shields.io/badge/Blog-FF5722?style=flat-square&logo=Blogger&logoColor=white"/></a><br>
+</p>
+
+---
+
+## Contact me
+
+jun109209@gmail.com
---
+++ readmeUpdate.js
@@ -0,0 +1,59 @@
+import { writeFileSync } from 'node:fs';
+import Parser from 'rss-parser';
+
+/**
+ * README.MD에 작성될 페이지 텍스트
+ * @type {string}
+ */
+let text = `# Hi there 👋
+
+[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=rkdden)](https://github.com/anuraghazra/github-readme-stats)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=rkdden&layout=compact&hide=r,jupyter%20notebook,c%23&exclude_repo=roharui.github.io)](https://github.com/anuraghazra/github-readme-stats)
+
+## 이런 환경에 익숙해요✍🏼
+
+## 언어
+
+<p>
+  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
+  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
+</p>
+
+## Contact me
+
+gangsuyun6@gmail.com
+
+## 📕 Latest Blog Posts
+<p>
+    <a href="https://jun10920.tistory.com/"><img src="https://img.shields.io/badge/Blog-FF5722?style=flat-square&logo=Blogger&logoColor=white"/></a><br>
+</p>
+
+`;
+
+// rss-parser 생성
+const parser = new Parser({
+  headers: {
+    Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
+  },
+});
+
+(async () => {
+  // 피드 목록
+  const feed = await parser.parseURL('https://jun10920.tistory.com//rss');
+
+  // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
+  for (let i = 0; i < 5; i++) {
+    const { title, link } = feed.items[i];
+    console.log(`${i + 1}번째 게시물`);
+    console.log(`추가될 제목: ${title}`);
+    console.log(`추가될 링크: ${link}`);
+    text += `<a href=${link}>${title}</a></br>`;
+  }
+
+  // README.md 파일 작성
+  writeFileSync('README.md', text, 'utf8', (e) => {
+    console.log(e);
+  });
+
+  console.log('업데이트 완료');
+})();
